470a75ed2753fb3310331d304c460adedf214039
MaraCross <mari.yan75@yahoo.com>
2022-02-19 23:21:16 +0400

    sqlite3 query

---
+++ pick.py
@@ -0,0 +1,159 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QMessageBox
+import sqlite3
+import sys
+
+class Ui_MainWindow(object):
+
+    def setupUi(self, MainWindow):
+        MainWindow.setFixedSize(1000, 900)
+        MainWindow.setObjectName("MainWindow")        
+        
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+
+       
+        
+        self.text=QtWidgets.QTextEdit(self.centralwidget)
+        self.text.setGeometry(QtCore.QRect(60, 50, 400, 50))
+        self.text.setObjectName("text")
+
+        self.push_btn = QtWidgets.QPushButton(self.centralwidget)
+        self.push_btn.setGeometry(QtCore.QRect(500, 50, 161, 51))
+        self.push_btn.setObjectName("push_btn")
+
+        self.text_1 = QtWidgets.QTextEdit(self.centralwidget)
+        self.text_1.setGeometry(QtCore.QRect(60, 120, 400, 250))
+        self.text_1.setObjectName("text_1")
+
+        self.push_btn_1 = QtWidgets.QPushButton(self.centralwidget)
+        self.push_btn_1.setGeometry(QtCore.QRect(500, 320, 161, 51))
+        self.push_btn_1.setObjectName("push_btn_1")
+        
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 24))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+        self.con=False
+        self.table_1=False
+
+    
+
+        self.add_functions()
+        
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "sqlite3 query"))
+        self.push_btn.setText(_translate("MainWindow", "CONNECT"))
+        self.push_btn_1.setText(_translate("MainWindow", "INPUT"))
+        self.text.setText(_translate("MainWindow",":memory:"))
+        
+
+
+    def add_functions(self):
+        self.push_btn.clicked.connect(lambda:self.db_connect(self.text.toPlainText()))
+        self.push_btn_1.clicked.connect(lambda:self.db_input(self.text_1.toPlainText()))
+    
+    def db_connect(self,txt1):
+
+        
+        self.db=sqlite3.connect(txt1)
+        connected_label = QtWidgets.QLabel(self.centralwidget)
+        connected_label.setGeometry(QtCore.QRect(700, 55, 150, 40))
+        font = QtGui.QFont()
+        font.setPointSize(13)
+        font.setWeight(40)
+       
+        connected_label.setFont(font)
+        connected_label.setLayoutDirection(QtCore.Qt.LeftToRight)
+        connected_label.setStyleSheet("background-color: rgb(88,88,88);")
+     
+        connected_label.setAlignment(QtCore.Qt.AlignCenter)
+    
+        connected_label.setObjectName("connected_label")
+        connected_label.setText(QtCore.QCoreApplication.translate("MainWindow", "connected "+txt1))
+
+        connected_label.show()
+        self.con=True
+        
+
+    def db_input(self,txt):
+        if self.con:
+            try:
+
+                with self.db as db:
+            
+                    c=db.cursor()
+                    if self.table_1:
+                        self.table.deleteLater()
+                        self.table_1=False
+                        
+
+                    c.execute(txt)
+                    
+                        
+            
+                    if 'select' in txt:
+                        a=c.fetchall()
+
+                        self.table = QtWidgets.QTableView(self.centralwidget)
+                        self.table.setGeometry(QtCore.QRect(60, 400, 800, 400))
+                        self.table.setObjectName("table")
+
+                        names=[description[0] for description in c.description] 
+
+                        model = QtGui.QStandardItemModel(len(a), len(names))
+                        for row in range(len(a)):
+                            for column in range(len(names)):
+                                item = QtGui.QStandardItem(str(a[row][column]))
+                                model.setItem(row, column, item)
+                        
+                        model.setHorizontalHeaderLabels(names)
+
+                        self.table.setModel(model)
+                        
+                        self.table.show()
+                        self.table_1=True
+                    
+
+
+                    db.commit()
+            except Exception as e:
+                error_1=QMessageBox()
+                error_1.setWindowTitle("Error")
+                error_1.setText(str(e))
+                error_1.setIcon(QMessageBox.Warning)
+                error_1.setStandardButtons(QMessageBox.Ok)
+                
+                error_1.exec_()
+
+        else:
+            error=QMessageBox()
+            error.setWindowTitle("Error")
+            error.setText("Please connect to DB")
+            error.setIcon(QMessageBox.Warning)
+            error.setStandardButtons(QMessageBox.Ok)
+            
+            error.exec_()
+
+
+         
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+
+    MainWindow.show()
+    sys.exit(app.exec_())
+
+
